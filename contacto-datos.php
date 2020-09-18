0c25143b5bbd350dd2d84bd87e0515c328ba8508
Cecilia Albero <calbero@itx.com>
2020-09-18 11:46:31 -0300

    Agrega favicon

--- contacto-datos.php
+++ contacto-datos.php
@@ -1,10 +1,68 @@
 <?php
-	$mensaje= "\nNombre: ".$_POST['nombre'];
-	$mensaje.= "\nE-mail: ".$_POST['email'];
-	$mensaje.= "\nTelefono: ".$_POST['telefono'];
-	$mensaje.= "\nConsulta: ".$_POST['consulta'];
-	$remitente = $_POST ['email'];
-	$asunto = "Mensaje enviado desde Loira web: ".$_POST['nombre'];
-	mail("ceci.albero@gmail.com",$asunto,$mensaje,"FROM: $remitente");
-	echo "Mensaje enviado. Gracias por contactarse.";
-?>
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+if (
+    !empty($_POST['nombre']) &&
+    !empty($_POST['email'])
+) {
+
+    //Configuración
+    $destino = "hola@loiracolegial.com.ar";
+    //
+
+    $nombre   = $_POST['nombre'];
+    $email    = $_POST['email'];
+		$telefono    = $_POST['telefono'];
+    $consulta = !empty($_POST['consulta']) ? $_POST['consulta'] : false;
+
+    require 'lib/PHPMailer.php';
+    require 'lib/SMTP.php';
+    require 'lib/Exception.php';
+
+    $mail = new PHPMailer(true); // Passing `true` enables exceptions
+    try {
+
+        $mensaje = "\nNombre: " . $nombre;
+        $mensaje .= "\nE-mail: " . $email;
+				$mensaje .= "\nTelefono: " . $telefono;
+        $mensaje .= "\nConsulta: " . $consulta;
+        $asunto = "Mensaje enviado desde Loira Web: " . $nombre;
+
+        //Server settings
+        $mail->isSMTP();
+        $mail->Host       = 'c1791309.ferozo.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'hola@loiracolegial.com.ar';
+        $mail->Password   = 'Loira2020';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port       = 465;
+
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer'       => false,
+                'verify_peer_name'  => false,
+                'allow_self_signed' => true,
+            ),
+        );
+
+        //Recipients
+        $mail->setFrom('no-reply@c1791309.ferozo.com', $nombre);
+        $mail->addAddress($destino);
+
+        //Content
+        $mail->isHTML(false);
+        $mail->Subject = $asunto;
+        $mail->Body    = $mensaje;
+        // $mail->AltBody = $mensaje;
+
+        $mail->send();
+        echo 1;
+    } catch (Exception $e) {
+    	echo 0;
+        error_log('Message could not be sent. Mailer Error: ', $mail->ErrorInfo);
+    }
+} else {
+	echo -1;
+}
--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+//
+function activeBotonNavbar() {
+    document.addEventListener('click', function () {
+
+        if (!event.target.classList.contains('nav-link')) return;
+        event.target.classList.add('active');
+        var links = document.querySelectorAll('.nav-link');
+        for (var i = 0; i < links.length; i++) {
+            if (links[i] === event.target) continue;
+            links[i].classList.remove('active');
+        }
+
+    }, false);
+}
+
+
 function cargaSendMail() {
     $("#c_enviar").attr("disabled", true);
     $(".c_error").remove();
@@ -59,3 +75,7 @@ function cargaSendMail() {
         $("#c_enviar").removeAttr("disabled");
     }
 }
+
+$(document).ready(function() {
+    activeBotonNavbar();
+});
